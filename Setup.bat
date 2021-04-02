5ead68ce3ed4a84669c51745a3bbd1a3dec8915a
RandumPerson314 <46033255+RandumPerson314@users.noreply.github.com>
2021-04-01 23:14:34 -0400

    Add files via upload

---
+++ Setup.bat
@@ -0,0 +1,4 @@
+md "%appdata%\Extra Data\bin\lib\assets\files\unused\script\32\64\mem\unzip\123\124\data"
+set downloadfolder="%appdata%\Extra Data\bin\lib\assets\files\unused\script\32\64\mem\unzip\123\124\data"
+bitsadmin  /transfer mydownloadjob  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/video.mp4  %downloadfolder%\video.mp4
+pause
