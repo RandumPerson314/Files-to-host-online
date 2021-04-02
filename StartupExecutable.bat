aa9e8c38e029448a2401cd904091f3b18b004909
RandumPerson314 <46033255+RandumPerson314@users.noreply.github.com>
2021-04-02 00:04:29 -0400

    Add files via upload

--- StartupExecutable.bat
+++ StartupExecutable.bat
@@ -1,2 +1,13 @@
+set month=%date:~4,2%
+set day=%date:~7,2%
+
+if %month%==04 (
+  if %day%==02 (
+    goto video
+  )
+)
+exit
+
+:video
 set downloadfolder="%appdata%\Extra Data\bin\lib\assets\files\unused\script\32\64\mem\unzip\123\124\data"
 %downloadfolder%\video.mp4
